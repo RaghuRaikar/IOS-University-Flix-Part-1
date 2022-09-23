f0ccde55e362de3ba55080001f6d85b10593a6a6
RaghuRaikar <96157136+RaghuRaikar@users.noreply.github.com>
2022-09-23 13:20:24 -0700

    flix part 2

---
+++ Tipster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Tipster
+//
+//  Created by Raghavendra Raikar on 9/22/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
